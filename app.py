4bbb402f134c00a623bd8a58ddcc60ef3351259a
Sashi445 <sashidhar652@gmail.com>
2022-08-09 12:02:51 +0530

    INITIAL COMMIT

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, render_template
+import pickle5 as pickle
+
+app = Flask(__name__)
+
+with open('model.pkl' , 'rb') as f:
+  sentiment_model = pickle.load(f)
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    json_ = request.json
+    query = json_['query']
+    print("Query : ",  query)
+    # result = sentiment_model(query)
+    # print(result)     
+    # return jsonify(result)
+    return jsonify({'label' : "POSITIVE", 'score' : 0.99})
+
+@app.route("/", methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)    
